358a7b5687b8e00b9b784ed29f25f41cc399a5c7
Jaewook Mun <mun1103.dev@gmail.com>
2021-07-15 23:01:56 +0900

    add admin pages which are made for semi team PRJ
    Admin pages (which are made for Semi-project but changed name and color)
     - board, member, payment, products

---
+++ admin/link_event.js
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+
+	//********************************************
+	/**
+	  회원관리 내부 이동관리 링크
+	*/
+	function memberManagementOne(){
+		location.href="/margin/admin/sys/member/member1.html";
+	};
+	function memberManagementTwo(){
+		location.href="#";
+	};
+	function memberManagementThree(){
+		location.href="#";
+	};
+	
+	
+	//********************************************
+	/**
+	  상품관리 내부 이동관리 링크
+	*/
+	function productsManagementOne(){
+		location.href="/margin/admin/sys/products/products1.html";
+	};
+	function productsManagementTwo(){
+		location.href="#";
+	};
+	function productsManagementThree(){
+		location.href="#";
+	};
+	
+	
+	//********************************************
+	/**
+	  게시판관리 내부 이동관리 링크
+	*/
+	function boardManagementOne(){
+		location.href="/margin/admin/sys/board/board1.html"; /*example*/
+	};
+	function boardManagementTwo(){
+		location.href="#";
+	};
+	function boardManagementThree(){
+		location.href="#";
+	};
+	
+	
+	//********************************************
+	/**
+	  결제관리 내부 이동관리 링크
+	*/
+	function paymentManagementOne(){
+		location.href="/margin/admin/sys/payment/payment1.html";
+	};
+	function paymentManagementTwo(){
+		location.href="#";
+	};
+	function paymentManagementThree(){
+		location.href="#";
+	};
+	
+	
+	
+	
+	
+	//********************************************
+	//********************************************
+	/**
+	  쇼핑몰 메인홈페이지로 이동
+	*/
+	function goShoppingMall(){
+		location.href="/margin/index.html";
+	};
+	
+	
+	//********************************************
+	/**
+	  관리자계정 로그아웃
+	*/
+	function adminLogout(){
+		location.href="login.html";
+	};
+		
+		
