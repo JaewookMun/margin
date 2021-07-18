cc0c81ad9daeae7488750f05f40052f923836d7b
Jaewook Mun <mun1103.dev@gmail.com>
2021-07-18 19:45:30 +0900

    Add [delete, update] function

--- java/web/data/BoardDAO.java
+++ java/web/data/BoardDAO.java
@@ -18,6 +18,8 @@ public class BoardDAO extends OracleConnectionPool {
 		return _instance;
 	}
 	
+	// check : Sequence(시퀀스) 초기화
+	
 	public int insertPost(BoardDTO post) {
 		Connection con=null;
 		PreparedStatement pstmt=null;
@@ -49,7 +51,7 @@ public class BoardDAO extends OracleConnectionPool {
 		List<BoardDTO> postList = new ArrayList<BoardDTO>();
 		try {
 			con=getConnection();
-			String sql="SELECT * FROM board_practice";
+			String sql="SELECT * FROM board_practice ORDER BY no DESC";
 			pstmt=con.prepareStatement(sql);
 			rs=pstmt.executeQuery();
 			
@@ -70,5 +72,88 @@ public class BoardDAO extends OracleConnectionPool {
 		}
 		
 		return postList;
+	}
+	
+	// 하나만 지울 경우
+	public int deletePost(int no) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int row=0;
+		
+		try {
+			con=getConnection();
+			String sql="DELETE FROM board_practice WHERE no=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, no);
+			
+			row=pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println("[deletePost()]sql error : "+e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		
+		return row;
+	}
+	
+	
+	/* 반복처리할 수 있음.
+	// 두개 이상 지울 경우
+	public int[] deletePosts(int[] noArray) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int[] posts=null;
+		try {
+			con=getConnection();
+			con.setAutoCommit(false);
+			String sql = "DELETE FROM board_practice WHERE no=?";
+			pstmt=con.prepareStatement(sql);
+			
+			for(int i=0; i<noArray.length; i++) {
+				pstmt.setInt(1, noArray[i]);
+				pstmt.addBatch();
+			}
+			posts = pstmt.executeBatch();
+			con.commit();
+			
+		} catch (SQLException e) {
+			System.out.println("[deletePosts()]sql error : "+e.getMessage());
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} finally {
+			close(con, pstmt);
+		}
+		
+		return posts;
+	}
+	*/
+	
+	
+	public int updatePost(int no, String content) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int row=0;
+		
+		try {
+			con=getConnection();
+			String sql="UPDATE board_practice SET content=? WHERE no=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, content);
+			pstmt.setInt(2, no);
+			
+			row=pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println("[updatePost()]sql error : "+e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		
+		return row;
 	}
 }
