563f6a4b52d0c9b8a5ea7ab9bb74b5c40459f0a2
JaewookMun <mun1103.dev@gmail.com>
2023-10-01 18:57:03 +0900

    remove unvalid data

--- gradeDemo/ScoreCalculation.java
+++
@@ -1,9 +0,0 @@
-package doit.project.gradecalculation;
-
-public interface ScoreCalculation {
-
-	int calcScore();
-	
-	String calcGradeRequired(int score);
-	String calcGradeUsual(int score);
-}
--- gradesys/calc/ScoreCalc.java
+++
@@ -1,8 +0,0 @@
-package doit.project.gradesys.calc;
-
-//사용하지 않는 메소드 삭제
-public interface ScoreCalc {
-	
-	String calcGrade(int score);
-	
-}
